0f6ff6b486d55d62db58a1f93213db206dc2f858
Vikas Kumar <vikask200202@gmail.com>
2025-06-26 09:06:19 +0530

    accpting a request and sending a responsoe to the client

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,39 +2,44 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import static java.lang.System.out;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    out.println("Logs from your program will appear here!");
+      try{
+          ServerSocket serverSocket= new ServerSocket(5000);
 
-//     Uncomment this block to pass the first stage
 
-     try {
-           ServerSocket serverSocket = new ServerSocket(4221);
+          System.out.println("accepting the connection from client");
+          Socket socket = serverSocket.accept();
 
-           // Since the tester restarts your program quite often, setting SO_REUSEADDR
-           // ensures that we don't run into 'Address already in use' errors
-           serverSocket.setReuseAddress(true);
+          System.out.println("parsing the http request from the server: ");
 
-           Socket clientSocket=serverSocket.accept(); // Wait for connection from client.
-           out.println("New client connected");
-           BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-           StringBuilder request = new StringBuilder();
-           String line = "";
-           while((line = in.readLine())!=null && !line.isEmpty())
-           {
-               request.append(line).append("\r\n");
-           }
-           out.println("received http request"+request);;
-           OutputStream outputStream= clientSocket.getOutputStream();
-           String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from Java Server!";
-           outputStream.write(response.getBytes());
-           outputStream.flush();
+          InputStream in = socket.getInputStream();
+          InputStreamReader inr= new InputStreamReader(in);
+          BufferedReader buff = new BufferedReader(inr);
+          String line = buff.readLine();
+          System.out.println("this is the request line : "+line);
+          String httpResponse = "";
+          if(line!=null && line.split(" ")[1].equals("/"))
+          {
+              httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+          }
+          else
+          {
+              httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+          }
 
-     } catch (IOException e) {
-       out.println("IOException: " + e.getMessage());
-     }
+
+          System.out.println("sending the response to the client");
+          OutputStream out = socket.getOutputStream();
+
+          out.write(httpResponse.getBytes("UTF-8"));
+
+          socket.close();
+          serverSocket.close();  // optional, to close server
+
+      } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
   }
 }
